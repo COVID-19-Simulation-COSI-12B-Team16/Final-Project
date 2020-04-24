50c407a82b93743d221db0d600a796cb9dce586d
yirunzhou <yirunzhou@brandeis.edu>
2020-04-23 21:34:19 -0400

    methods and comments for Board and Cell

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,47 @@
+import java.util.List;
+
 public class Board {
+    private int row;
+    private int col;
+    private Cell[][] cells;
+
+    Board(int _row, int _col){
+        row = _row;
+        col = _col;
+        cells = new Cell[row][col];
+    }
+
+    /**
+     * Initialize with random cells, alive or dead
+     */
+    private void init(){
+    }
+
+    /**
+     * Start clock
+     */
+    void start(){
+
+    }
+
+    /**
+     * Stop clock
+     */
+    void stop(){
+
+    }
+
+    /**
+     * Render the board on canvas
+     */
+    private void draw(){
+
+    }
+
+    /**
+     * Update cells on the board to next generation
+     */
+    private void moveToNextGeneration(){
+
+    }
 }
--- src/Cell.java
+++ src/Cell.java
@@ -1,2 +1,21 @@
 public class Cell {
+    private boolean isAlive;
+    private boolean nextAlive;
+
+    private int x;
+    private int y;
+
+    /**
+     * Update nextAlive attribute according to surrounding cells' conditions
+     */
+    void updateNextAlive(Cell[][] cells){
+
+    }
+
+    /**
+     * Update the cell to next generation according to nextAlive attribute derived from above method
+     */
+    void nextGeneration(){
+
+    }
 }
